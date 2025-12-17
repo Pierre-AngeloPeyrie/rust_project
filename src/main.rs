c56dbb114dea519d98f020fe5c8520732ec781c8
Pierre-angeloPeyrie <papeyrie@proton.me>
2025-12-17 11:30:24 +0100

    collisions working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use std::iter::zip;
-use std::str::ParseBoolError;
-
 use ggez::event;
 use ggez::graphics::{self, Color};
 use ggez::{Context, GameResult};
@@ -11,6 +8,9 @@ struct MainState {
     positions: Vec<Vec2>,
     prev_positions: Vec<Vec2>,
     gravity: Vec2,
+    space_center: Vec2,
+    space_radius: f32,
+    particle_radius: f32,
 }
 
 impl MainState {
@@ -18,7 +18,11 @@ impl MainState {
         let s = MainState{
             positions: Vec::new(),
             prev_positions: Vec::new(),
-            gravity: Vec2::new(0., 300.)
+            gravity: Vec2::new(0., 300.),
+            space_center: Vec2::new(400., 300.),
+            space_radius: 290.,
+            particle_radius: 20.,
+
         };
         Ok(s)
     }
@@ -29,51 +33,49 @@ impl MainState {
     }
 
     fn update_positions(&mut self, dt: f32){
-        for i in 0..self.positions.len() {
+        for i in 0..self.positions.len(){
             let velocity = self.positions[i] - self.prev_positions[i];
             self.prev_positions[i] = self.positions[i];
             self.positions[i] += velocity + self.gravity * dt * dt;
         }        
     }
 
-    fn constraint(&mut self, win_width: f32, win_height: f32){
-        //let velocity = self.position - self.prev_position;
+    fn collisions(&mut self){
+        for i in 0..self.positions.len(){
+            for j in i+1..self.positions.len(){
+                let collision_vector = self.positions[i] - self.positions[j];
+                let distance = collision_vector.length();
+                if distance < 2. * self.particle_radius{
+                    let delta = 2. * self.particle_radius - distance;
+                    let n = collision_vector/distance;
+                    self.positions[i] += 0.5 * delta * n;
+                    self.positions[j] -= 0.5 * delta * n;
+                }
+            }
+        }
+    }
 
-       /*  if self.position.x - 50. < 0.{
-            self.position.x = 50.;
-            self.prev_position.x = self.position.x + velocity.x;
-
-        } else if self.position.x  + 50. > win_width{
-            self.position.x = win_width - 50.;
-            self.prev_position.x = self.position.x + velocity.x;
-        };
-
-        if self.position.y - 50. < 0.{
-            self.position.y = 50.;
-            self.prev_position.y = self.position.y + velocity.y;
-
-        }else if self.position.y + 50. > win_height{
-            self.position.y = win_height - 50.;
-            self.prev_position.y = self.position.y + velocity.y;
-        }; */
-
-        const CENTER: Vec2 = Vec2::new(400., 100.);
-        let to_obj = self.position - CENTER;
-        let dist = to_obj.length();
-        if dist > 300.{
-            self.position = CENTER + to_obj/dist * 300.
-        }   
+    fn constraint(&mut self){
+        for i in 0..self.positions.len(){
+            let center_to_part= self.positions[i] - self.space_center;
+            let distance = center_to_part.length();
+            if distance > self.space_radius - self.particle_radius {
+                self.positions[i] = self.space_center + center_to_part/distance * (self.space_radius - self.particle_radius)
+            }   
+        }
+        
     }
 }
 
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        let win_width = ctx.gfx.window().inner_size().width as f32;
-        let win_height = ctx.gfx.window().inner_size().height as f32;  
-
-        self.objects.iter_mut().for_each(|particle| particle.accelerate(self.gravity));
-        self.objects.iter_mut().for_each(|particle| particle.update(ctx.time.delta().as_secs_f32()));
-        self.objects.iter_mut().for_each(|particle| particle.constraint(win_width, win_height));
+        let sub_steps = 4;
+        for _ in 0..sub_steps{
+            self.update_positions(ctx.time.delta().as_secs_f32()/sub_steps as f32);
+            self.constraint();
+            self.collisions();
+        }
+        
 
         Ok(())
     }
@@ -83,16 +85,25 @@ impl event::EventHandler<ggez::GameError> for MainState {
             ctx,
             graphics::Color::from([0.1, 0.2, 0.3, 1.0]),
         );
+        let space_mesh = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            Vec2::new(0.0, 0.0),
+            self.space_radius,
+            0.1,
+            Color::BLACK,
+        )?;
 
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
             Vec2::new(0.0, 0.0),
-            50.0,
+            self.particle_radius,
             0.1,
             Color::WHITE,
         )?;
-        self.objects.iter_mut().for_each(|particle| canvas.draw(&circle, particle.position));
+        canvas.draw(&space_mesh, self.space_center);
+        self.positions.iter().for_each(|position| canvas.draw(&circle, *position));
 
         canvas.finish(ctx)?;
         Ok(())
@@ -105,7 +116,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
                 Ok(())
             },
             Some(KeyCode::Space) =>{
-                self.objects.push(Particle::new(Vec2::new(500., 100.)));
+                self.add_particle(Vec2::new(600., 200.));
                 Ok(())
             },
             _ => Ok(()), // Do nothing
